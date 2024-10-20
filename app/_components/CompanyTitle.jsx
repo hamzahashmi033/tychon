86b0212a39ff05f0293fbe19028e85c0144e0700
hamzahashmi033 <hashmihamza846@gmail.com>
2024-10-20 06:27:18 -0700

    test deployemnt

--- app/_components/CompanyTitle.jsx
+++ app/_components/CompanyTitle.jsx
@@ -66,9 +66,9 @@ const CompanyTitle = (props) => {
       </Float>
       <Stars
         radius={100}
-        depth={100}
+        depth={50}
         count={6000}
-        factor={4}
+        factor={6}
         saturation={0}
         fade
         speed={2}
